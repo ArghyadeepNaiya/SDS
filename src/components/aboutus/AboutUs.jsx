41e3a13b8fbe02f70efbb642f4b150cb0277c88e
ArghyadeepNaiya <naiyaarghyadeep@gmail.com>
2026-06-20 02:39:12 +0530

    created the about us container with all functionality with responsive styling,

--- src/components/aboutus/AboutUs.jsx
+++ src/components/aboutus/AboutUs.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import AboutUsText from './AboutUsText'
 import './aboutus.css'
+import {backOut, motion} from 'motion/react'
 const AboutUs = () => {
-  return (
-    <div className='flex flex-col items-center justify-center w-full py-20'>
-        <AboutUsText></AboutUsText>
-    </div>
-  )
+    return (
+        <div className='flex flex-col items-center justify-center w-full py-20'>
+            <motion.h1 className='flex justify-center
+             text-6xl text-white'
+             initial={{opacity:0,y:0,backgroundColor:"none",}}
+             whileInView={{opacity:1,y:0,backgroundColor:"rgb(218,171,0)",color:"rgb(0,0,0)"}}
+             transition={{ease:"backout",duration:0.7}} id='about'>
+                About Us
+            </motion.h1>
+            <AboutUsText normal={"Society for Data Science is BIT Mesra's first student-run, not-for-profit organization focused on data science and its interdisciplinary applications."}
+                mask={"Since our founding in 2019, we have united students from a wide range of disciplines behind our shared passion for all things data. Our mission is to foster a strong data science community on campus. Each semester, we focus on: developing educational learning experiences like our technical workshops and decal course to expand data science education on campus, hosting professional events for students to connect with industry leaders and grow their network, and conducting analytics consulting projects to help members gain experience and make an impact. Our program provides over a hundred students each semester with mentorship and data science skills through working in teams on a tangible group project. We aim to build an inclusive and accessible community where students of all levels and backgrounds can dive into the world of data science."}>
+
+            </AboutUsText>
+            <AboutUsText normal={"We have everything you need to launch and to grow your academic environment."}
+            mask={"The Society for Data Science is a non-profit society of data science enthusiasts that serves our members, improving the data science profession, eliminating bias and enhancing diversity, and advancing ethical data science throughout the world."}
+            >
+
+            </AboutUsText>
+        </div>
+    )
 }
 
 export default AboutUs
--- src/components/aboutus/AboutUsText.jsx
+++ src/components/aboutus/AboutUsText.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './aboutustext.css'
-const AboutUsText = () => {
+import {backOut, motion} from 'motion/react'
+
+
+const AboutUsText = ({mask,normal}) => {
+    //define cursor coordinate
+    const [mousePosition,SetMousePosition]=useState({x:0,y:0})
+    const containerRef = React.useRef(null);
+
+    //define if mouse is hovered on element
+    const [isHovered,setIsHovered]=useState(false)
+
+    const handleMouseMove = (e) => {
+        if (!containerRef.current) return;
+        const rect = containerRef.current.getBoundingClientRect();
+        SetMousePosition({
+            x: e.clientX - rect.left,
+            y: e.clientY - rect.top
+        });
+    };
+
+    const size = isHovered ? '650' : '100';
+
   return (
-    <div className="">
-        <h1 className='text-4xl text-white font-bold'>
-            hello world
-        </h1>
+    <div className="container" ref={containerRef} onMouseMove={handleMouseMove}>
+        
+        <motion.div className='normal'>
+            <motion.h1
+            initial={{opacity:0,y:20}}
+            whileInView={{opacity:1,y:0}}
+            transition={{delay:0.5}}>
+                {normal}
+            </motion.h1>
+        </motion.div>
+        <motion.div className='mask'
+        animate={{webkitMaskPosition:`${mousePosition.x-size/2}px ${mousePosition.y-size/2}px`,
+            webkitMaskSize:`${size}px`,
+        }}
+        transition={{ease:"backOut",duration:0.4}}>
+            <motion.h1
+            initial={{opacity:0,y:20}}
+            whileInView={{opacity:1,y:0}}
+            transition={{delay:0.8}}
+             onMouseEnter={()=>setIsHovered(true)}
+             onMouseLeave={()=>setIsHovered(false)}
+             >
+                {mask}
+            </motion.h1>
+        </motion.div>
+        
     </div>
   )
 }
