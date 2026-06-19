dc461afd34667b57b191e0491043dfeb4b0bd843
ArghyadeepNaiya <naiyaarghyadeep@gmail.com>
2026-06-19 19:34:57 +0530

    made the navbar working and responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,19 +4,23 @@ import menuImg from '../../assets/menu.svg'
 import closeImg from '../../assets/close.svg'
 
 const Navbar = () => {
-    const [toggle,setToggle]=useState(false)
+    const [toggle, setToggle] = useState(false)
+    
   return (
     <div id="navbar">
         <h2 id='title'>SDS</h2>
-        <ul id="links">
+        
+        {/* We add an 'active' class when toggle is true */}
+        <ul id="links" className={toggle ? "active" : ""}>
             <li><a href='#home'>Home</a></li>
             <li><a href='#about'>About Us</a></li>
             <li><a href='#projects'>Projects</a></li>
             <li><a href='#contact'>Contact</a></li>
         </ul>
-        <button onClick={()=>{setToggle(!toggle)}}
-        id='toggle'>
-            <img src={!toggle?menuImg:closeImg} alt=''/>
+        
+        <button onClick={() => setToggle(!toggle)} id='toggle'>
+            {/* Swap between menu and close icons */}
+            <img src={toggle ? closeImg : menuImg} alt='Menu' width="28" height="28"/>
         </button>
     </div>
   )
