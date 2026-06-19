6ca389d2acf60f2a3cc04ed45fdc0be9122ff127
ArghyadeepNaiya <naiyaarghyadeep@gmail.com>
2026-06-19 19:15:32 +0530

    added the toggleing switch in the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
+import menuImg from '../../assets/menu.svg'
+import closeImg from '../../assets/close.svg'
 
 const Navbar = () => {
+    const [toggle,setToggle]=useState(false)
   return (
     <div id="navbar">
-        <h2>SDS</h2>
+        <h2 id='title'>SDS</h2>
         <ul id="links">
             <li><a href='#home'>Home</a></li>
             <li><a href='#about'>About Us</a></li>
             <li><a href='#projects'>Projects</a></li>
             <li><a href='#contact'>Contact</a></li>
         </ul>
+        <button onClick={()=>{setToggle(!toggle)}}
+        id='toggle'>
+            <img src={!toggle?menuImg:closeImg} alt=''/>
+        </button>
     </div>
   )
 }
