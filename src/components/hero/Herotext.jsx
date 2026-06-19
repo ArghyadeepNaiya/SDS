55c4e1f1042fd0afa1b2c346e2f10e329dcf0032
ArghyadeepNaiya <naiyaarghyadeep@gmail.com>
2026-06-19 20:33:08 +0530

    added the flipword mechanism in the herotext now will work on animating these things

--- src/components/hero/Herotext.jsx
+++ src/components/hero/Herotext.jsx
@@ -1,16 +1,19 @@
+import { FlipWords } from '../FlipWords'
 import './herotext.css'
 import React from 'react'
 
+
 const Herotext = () => {
+    const words=["Create.","Innovate.","Inspire."]
   return (
     <div>
         <div id='welcomeMsg'>
-            <h1 className='text-5xl text-white' >
+            <h1 className='text-7xl text-white' >
             Welcome to <br>
             </br>Society for <br>
-            </br> DaTa Science.
+            </br> Data Science.
             <h1>
-                We 
+                We <FlipWords words={words}></FlipWords>
             </h1>
         </h1>
         </div>
