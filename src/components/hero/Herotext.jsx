0c37e1bfbc8b9a7bef290d8d5cd5297eaec6ae53
ArghyadeepNaiya <naiyaarghyadeep@gmail.com>
2026-06-19 21:23:55 +0530

    added motion effect in the herotext.jsx

--- src/components/hero/Herotext.jsx
+++ src/components/hero/Herotext.jsx
@@ -1,4 +1,5 @@
 import { FlipWords } from '../FlipWords'
+import { motion, scale } from 'motion/react'
 import './herotext.css'
 import React from 'react'
 
@@ -7,16 +8,28 @@ const Herotext = () => {
     const words=["Create.","Innovate.","Inspire."]
   return (
     <div>
-        <div id='welcomeMsg'>
-            <h1 className='text-7xl text-white' >
+        <motion.div id='welcomeMsg'
+        initial={{opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{delay:0.2}}>
+            <motion.h1 className='text-7xl 
+            text-white' 
+            initial={{opacity:0,x:-50}}
+            animate={{opacity:1,x:0,}}
+            transition={{delay:0.5}}>
             Welcome to <br>
             </br>Society for <br>
             </br> Data Science.
-            <h1>
+            
+        </motion.h1>
+        <motion.h1 className='text-white text-7xl'
+        initial={{opacity:0,x:-30,}}
+        animate={{opacity:1,x:0}}
+        transition={{delay:0.7}}
+        >
                 We <FlipWords words={words}></FlipWords>
-            </h1>
-        </h1>
-        </div>
+            </motion.h1>
+        </motion.div>
     </div>
   )
 }
