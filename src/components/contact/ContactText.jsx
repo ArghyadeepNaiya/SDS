92627368cc1babf9a409623c7720fa825b918e93
ArghyadeepNaiya <naiyaarghyadeep@gmail.com>
2026-06-21 02:31:55 +0530

    changing the design and functionality of the contact component

--- src/components/contact/ContactText.jsx
+++ src/components/contact/ContactText.jsx
@@ -8,15 +8,15 @@ import {motion} from 'motion/react'
 const ContactText = () => {
   return (
     <div id='container'>
-        <h1 className='text-amber-400 text-5xl' id='title'>
+        <h1 className='text-amber-400 text-4xl' id='title'>
             Contact Us
         </h1>
         <form className='text-white' id='info'>
             
-            <input type='textbox' className='text-white' placeholder='Your Name'></input>
-            <input type='textbox' className='text-white' placeholder='E-mail'></input>
-            <input type='textbox' className='text-white' placeholder='More information'></input>
-            <input type='submit' placeholder='Submit' className='bg-amber-400 text-black text-2xl font-medium'></input>
+            <input type='text' className='text-white' placeholder='Your Name' required></input>
+            <input type='email' className='text-white' placeholder='E-mail'></input>
+            <textarea type='textbox' className='text-white' placeholder='More information'></textarea>
+            <button type='submit' id='submit'>Submit</button>
         </form>
         <footer id='footer'>
 
