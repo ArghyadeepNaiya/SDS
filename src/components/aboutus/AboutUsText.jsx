11a176b9f57142c88f332b5eb784dd50655ce8a9
ArghyadeepNaiya <naiyaarghyadeep@gmail.com>
2026-06-21 19:50:46 +0530

    changed the mask size from 550 to 650

--- src/components/aboutus/AboutUsText.jsx
+++ src/components/aboutus/AboutUsText.jsx
@@ -20,7 +20,7 @@ const AboutUsText = ({mask,normal}) => {
         });
     };
 
-    const size = isHovered ? '550' : '100';
+    const size = isHovered ? '650' : '100';
 
   return (
     <div className="container" ref={containerRef} onMouseMove={handleMouseMove}>
